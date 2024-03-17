272bf415ac58deccdb68d3f77a24b328e069ee81
Delgorithm <delgorithm@gmail.com>
2024-03-17 22:00:11 +0100

    Fix : script for the style of the button

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const allBtn = document.querySelectorAll('.filterRestaurant');
 
-
 allBtn.forEach((btn) => {
     btn.addEventListener('click', () => {
         allBtn.forEach((button) => {
             button.style.backgroundColor = '';
             button.style.color = '';
-        })
+            button.style.scale = '1.0';
+        });
 
         btn.style.backgroundColor = 'var(--clr-main-pink)';
         btn.style.color = 'var(--clr-white)';
         btn.style.scale = "1.1"
-    })
-})
+    });
+});
